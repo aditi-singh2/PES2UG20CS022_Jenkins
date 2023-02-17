79b8c2f7adcc1e7cf435fb3630dc31b2d7f1c28d
Aditi Singh <91794692+aditi-singh2@users.noreply.github.com>
2023-02-17 22:38:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I have successfully created a webhook!\n";
   cout << "SRN:PES2UG20CS022\n";
   return 0;
 }
